971a317753fa887507d1cdbf5a24fc24f52d5f5b
Mrshen <1220974104@qq.com>
2022-09-23 15:34:38 +0800

    modify i18n

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,19 +6,38 @@ import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
 
 import styles from './index.module.css';
+import Translate from '@docusaurus/Translate';
 
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <h1 className="hero__title">
+          {/* <Translate>{siteConfig.title}</Translate> */}
+          <Translate
+            id="homepage.content.title"
+            description="homepage content title">
+            My Site
+          </Translate>
+        </h1>
+        <p className="hero__subtitle">
+          {/* <Translate>{siteConfig.tagline}</Translate> */}
+          <Translate
+            id="homepage.content.tagline"
+            description="homepage content tagline">
+            Dinosaurs are cool
+        </Translate>
+        </p>
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            <Translate
+              id="homepage.content.tutorial"
+              description="homepage content tutorial">
+              Docusaurus Tutorial - 5min ⏱️
+            </Translate>
           </Link>
         </div>
       </div>
